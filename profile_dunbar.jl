8fd1ac781aa00d0a273c807e7832ebf85bb8f26e
bkaperick <bryankaperick@gmail.com>
2020-04-12 18:52:24 +0200

    Improved timing code (not really profiling in any meaningful sense yet.  Gives us a framework to continue iterating and ensure we're not doing anything stupid, as well as to compare progress.

--- profile_dunbar.jl
+++ profile_dunbar.jl
@@ -1,4 +1,6 @@
 using TimerOutputs
+#using BenchmarkTools
+include("old_dunbar.jl")
 include("dunbar.jl")
 
 function profileBitItByN(k::Int64, nRange, T)
@@ -20,32 +22,98 @@ function profileBitItByK(n::Int64, kRange, T)
   end
   return to
 end
-function profileProportionAreGossipable(n::Int64, T)
-  kMin = Int64(ceil(1.5*(n-1)))
-  println("k=$(kMin)")
-  return profileProportionAreGossipable(n,kMin,T)
-end
-function profileProportionAreGossipable(n::Int64, k::Int64, T)
+
+"""
+    profileOldPAG(n::Int64, k::Int64, T::Int64)
+
+Profile old iterations of `ProportionAreGossipable(n,k)` in `old_dunbar.jl`
+with the latest in `dunbar.jl`.
+"""
+function profileOldPAG(n::Int64, k::Int64, T)
   kMin = Int64(ceil(1.5*(n-1)))
   to = TimerOutput()
-  println("burn in:")
-  proportionAreGossipable3(n,k)
-  proportionAreGossipable2(n,k)
-  proportionAreGossipable1(n,k)
-  println("finished burn")
+  #burn-in to make sure everything is compiled
+  proportionAreGossipable(5,7)
+  proportionAreGossipable2(5,7)
+  proportionAreGossipable1(5,7)
   for t=1:T
     @timeit to "pag1($(n),$(k))" proportionAreGossipable1(n,k)
-    @timeit to "pag3($(n),$(k))" proportionAreGossipable3(n,k)
     @timeit to "pag2($(n),$(k))" proportionAreGossipable2(n,k)
+    @timeit to "pag3($(n),$(k))" proportionAreGossipable(n,k) # latest
   end
   return to
 end
 
+"""
+    profileMinPAG(n::Int64, T::Int64)
+
+Profile `ProportionAreGossipable(n,k)` for all combinatorially-unique values 
+of `k` with `T` trials. 
+"""
+function profileMinPAG(nRange::OrdinalRange{Int64}, T)
+  proportionAreGossipable(5,7)
+  to = TimerOutput()
+  for n in nRange
+    profileMinPAG(n,to,T)
+    println(to)
+  end
+end
+
+"""
+    profileMinPAG(n::Int64, to::TimerOutput, T::Int64)
+
+Profile `ProportionAreGossipable(n,k)` updating `to` for minimum non-trivial 
+value of `k` with `T` trials. 
+"""
+function profileMinPAG(n::Int64,to::TimerOutput,T)
+  kMin = Int64(ceil(1.5*(n-1)))
+  return profilePAG(n,kMin,to,T)
+end
+
+"""
+    profilePAG(n::Int64, T::Int64)
+
+Profile `ProportionAreGossipable(n,k)` for all combinatorially-unique values 
+of `k` with `T` trials. 
+"""
+function profilePAG(n::Int64, T)
+  kMin = Int64(ceil(n*(n-1)/4))
+  kMax = Int64(n*(n-1)/2)
+  return profilePAG(n,kMax:-1:kMin,T)
+end
+
+"""
+    profilePAG(n::Int64,kRange::OrdinalRange{Int64}, T::Int64)
+
+Profile `ProportionAreGossipable(n,k)` for all values of `k` in `kRange` with 
+`T` trials. 
+"""
+function profilePAG(n::Int64,kRange::OrdinalRange{Int64}, T)
+  proportionAreGossipable(5,7)
+  to = TimerOutput()
+  for k in kRange
+    profilePAG(n,k,to,T)
+    println(to)
+  end
+  return to
+end
+
+"""
+    profilePAG(n::Int64, k::Int64, to::TimerOutput, T::Int64)
+
+Profile `ProportionAreGossipable(n,k)` updating `to` with `T` trials. 
+"""
+function profilePAG(n::Int64, k::Int64, to::TimerOutput, T)
+  for t=1:T
+    nP = numPaths(n,k)
+    @timeit to "pag($(n),$(k))($(nP))" proportionAreGossipable(n,k)
+  end
+end
 
 #println(profileBitItByK(32,2:2:8,1))
 #println(profileBitItByN(4,4:4:12,2))
-println(profileProportionAreGossipable(5,5))
-#println(profileProportionAreGossipable(6,1))
+#println(profilePAG(5,5))
+#println(profilePAG(6,1))
 
 #using Profile
 #@time proportionAreGossipable3(6,8)
@@ -54,3 +122,6 @@ println(profileProportionAreGossipable(5,5))
 #@profile @time proportionAreGossipable3(6,8)
 #
 #@time proportionAreGossipable3(5,1)
+#open("Readme.md","a") do io
+#  println(io,profilePAG(7,3));
+#end
