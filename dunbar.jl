668bc8e4eb0635ac1c259ca0d4975cd5ac079d31
bkaperick <bryankaperick@gmail.com>
2020-04-12 15:03:14 +0200

    Added iterator version of graph generation code.  Profiling suggests there is something wrong that makes it significantly slower than the other two.  Still investigating.

--- dunbar.jl
+++ dunbar.jl
@@ -139,7 +139,7 @@ end
 Constructs the graph data structure from a set of string node names `nodes` and
 a set of 2-tuple relations stored in the iterable `edges`.
 """
-function initializeGraph(nodes, edges)
+function initializeGraph(nodes, edges)::Dict{String, Array{String,1}}
   graph = []
   for node in nodes
     outEdges = []
@@ -185,13 +185,66 @@ function isGossipable(G)
   return all([isInTriangle(G,n) for (n,es) in G])
 end
 
+mutable struct GraphIt
+  n::Int64
+  k::Int64
+  nodes::Array{String,1}
+  allEdges::Array{Tuple{String,String},1}
+  bitArrays::BitIt
+  bitState::Tuple{Array{Bool,1},Int64}
+  start::Dict{String, Array{String,1}}
+end
+
+function GraphIt(n,k)
+  numEdges = Int64(n*(n-1)/2)
+  nodes = [string(Char(64+x)) for x in range(1,stop=n)]
+  
+  # Build array of all possible undirected edges, length (n-1)*n/2 
+  #allEdges = Tuple{String,String}[]
+  allEdges = []
+  for (i,node) in enumerate(nodes)
+    allEdges = vcat(allEdges, [(node,m) for m in nodes[i+1:end]])
+  end
+
+  # initialize bit iterator
+  bi = BitIt(numEdges,k)
+  bitStart,bitState = iterate(bi)
+  #bitState = (bitStart,0)
+
+  start = initializeGraph(nodes, allEdges[bitStart])
+  return GraphIt(n,k,nodes,allEdges,bi,bitState,start)
+end
+
+function Base.iterate(gi::GraphIt, state=(gi.start, 0))
+  elem,count = state
+  if count >= Base.length(gi.bitArrays)
+    return nothing
+  end
+   
+  # get pointers to current edges selected
+  next = iterate(gi.bitArrays, gi.bitState)
+  if (next == nothing)
+    return (elem, (elem,count+1))
+  else gi.bitState = next[2] end
+  schema = next[1]
+  
+  # get edges selected
+  currentEdges = gi.allEdges[schema]
+
+  # build graph and return state
+  G = initializeGraph(gi.nodes, currentEdges)
+  return (elem, (G, count + 1))
+end
+
+
+
 """
     generateGs(n, k)
 
 Take in the number of nodes `n` and the number of allowed edges `k`.  All 
 possible graphs with these parameters are iterated through.
 """
-function generateGs(n,k)
+function generateGs1(n,k)
   # Get generic node names
   nodes = [string(Char(64+x)) for x in range(1,stop=n)]
   
@@ -200,20 +253,42 @@ function generateGs(n,k)
   for (i,n) in enumerate(nodes)
     allEdges = vcat(allEdges, [(n,m) for m in nodes[i+1:end]])
   end
-  println("assembled edges: ", length(allEdges))
+  #println("assembled edges: ", length(allEdges))
   numEdges = length(allEdges)
   Gs = []
   bitArrays = generateActiveBitArrays(numEdges, k)
-  println("gotten bit arrays: ", length(bitArrays))
+  #println("gotten bit arrays: ", length(bitArrays))
 
   for schema in bitArrays
     currentEdges = allEdges[schema]
     G = initializeGraph(nodes, currentEdges)
     Gs = vcat(Gs, G)
   end
-  println("initialized graphs")
-  println(length(Gs))
+  #println("initialized graphs")
+  #println(length(Gs))
   #println("returning: ", Gs)
+  return Gs
+end
+function generateGs2(n,k)
+  # Get generic node names
+  nodes = [string(Char(64+x)) for x in range(1,stop=n)]
+  
+  # Build array of all possible undirected edges, length (n-1)*n/2 
+  allEdges = []
+  for (i,n) in enumerate(nodes)
+    allEdges = vcat(allEdges, [(n,m) for m in nodes[i+1:end]])
+  end
+  #println("assembled edges: ", length(allEdges))
+  numEdges = length(allEdges)
+  Gs = []
+  bitArrays = BitIt(numEdges, k)
+  #println("gotten bit arrays: ", length(bitArrays))
+
+  for (i,schema) in enumerate(bitArrays)
+    currentEdges = allEdges[schema]
+    G = initializeGraph(nodes, currentEdges)
+    Gs = vcat(Gs, G)
+  end
   return Gs
 end
 
@@ -223,7 +298,7 @@ end
 Returns the proportion of all possible graphs with `n` nodes and `k` edges
 which are gossipable.
 """
-function proportionAreGossipable(n, k)
+function proportionAreGossipable1(n, k)
   # easily-proven lower bound
   if (k < 1.5*(n-1))
     return 0.0
@@ -231,7 +306,39 @@ function proportionAreGossipable(n, k)
 
   count = 0
   total = 0
-  for G in generateGs(n,k)
+  for G in generateGs1(n,k)
+    count += isGossipable(G)
+    total += 1
+  end
+  #println(count, " / ", total)
+  return count / total
+end
+function proportionAreGossipable2(n, k)
+  # easily-proven lower bound
+  if (k < 1.5*(n-1))
+    println("safely ignored")
+    return 0.0
+  end
+
+  count = 0
+  total = 0
+  for G in generateGs2(n,k)
+    count += isGossipable(G)
+    total += 1
+  end
+  println(count, " / ", total)
+  return count / total
+end
+function proportionAreGossipable3(n, k)
+  # easily-proven lower bound
+  if (k < 1.5*(n-1))
+    println("safely ignored")
+    return 0.0
+  end
+
+  count = 0
+  total = 0
+  for G in GraphIt(n,k)
     count += isGossipable(G)
     total += 1
   end
--- profile_dunbar.jl
+++ profile_dunbar.jl
@@ -20,6 +20,37 @@ function profileBitItByK(n::Int64, kRange, T)
   end
   return to
 end
+function profileProportionAreGossipable(n::Int64, T)
+  kMin = Int64(ceil(1.5*(n-1)))
+  println("k=$(kMin)")
+  return profileProportionAreGossipable(n,kMin,T)
+end
+function profileProportionAreGossipable(n::Int64, k::Int64, T)
+  kMin = Int64(ceil(1.5*(n-1)))
+  to = TimerOutput()
+  println("burn in:")
+  proportionAreGossipable3(n,k)
+  proportionAreGossipable2(n,k)
+  proportionAreGossipable1(n,k)
+  println("finished burn")
+  for t=1:T
+    @timeit to "pag1($(n),$(k))" proportionAreGossipable1(n,k)
+    @timeit to "pag3($(n),$(k))" proportionAreGossipable3(n,k)
+    @timeit to "pag2($(n),$(k))" proportionAreGossipable2(n,k)
+  end
+  return to
+end
 
-println(profileBitItByK(32,2:2:8,1))
-println(profileBitItByN(4,4:4:12,2))
+
+#println(profileBitItByK(32,2:2:8,1))
+#println(profileBitItByN(4,4:4:12,2))
+println(profileProportionAreGossipable(5,5))
+#println(profileProportionAreGossipable(6,1))
+
+#using Profile
+#@time proportionAreGossipable3(6,8)
+#Profile.init(delay=0.01)
+#Profile.clear()
+#@profile @time proportionAreGossipable3(6,8)
+#
+#@time proportionAreGossipable3(5,1)
