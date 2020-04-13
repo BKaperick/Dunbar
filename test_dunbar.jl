658b8bb6164859e84655268f38d2268f5cf35c76
bkaperick <bryankaperick@gmail.com>
2020-04-13 23:24:08 +0200

    Updated test_dunbar.jl for latest code changes.

--- test_dunbar.jl
+++ test_dunbar.jl
@@ -1,20 +1,23 @@
 include("dunbar.jl")
 
-# Paths enumerating tests
-for n=1:8
-  for k=0:n
-    correctAns = paths(n,k)
-    @assert correctAns == length(generateActiveBitArrays(n,k))
-    @assert correctAns == length(collect(BitIt(n,k)))
-  end
-end
-
 # Graph tests
-nodes = ["a","b","c","d"]
-edges = [("a","b"),("b","c"),("c","a"),("a","d")]
-G = initializeGraph(nodes, edges)
+n = 5
+edges = [1,1,0,0,1,0,0,1,0,1]
+G = initializeGraph(n, edges)
 @assert !isGossipable(G)
-@assert isInTriangle(G,"a")
-@assert isInTriangle(G,"b")
-@assert isInTriangle(G,"c")
-@assert !isInTriangle(G,"d")
+@assert isInTriangle(G,1)
+@assert isInTriangle(G,2)
+@assert isInTriangle(G,3)
+@assert !isInTriangle(G,4)
+@assert !isInTriangle(G,5)
+
+@assert abs(proportionAreGossipable(3,2) - 0.0) < 1e-6
+@assert abs(proportionAreGossipable(3,3) - 1.0) < 1e-6
+@assert abs(proportionAreGossipable(6,8) - 0.08391608391608392) < 1e-6
+@assert abs(proportionAreGossipable(7,9) - 0.011193141224100976) < 1e-6
+
+# inclusive bounds for range of k-values that need be checked
+bounds(n) = (ceil(n*(n-1)/4),0.5*n^2 - 1.5*n+2)
+bsize(n) = bounds(n)[2] - bounds(n)[1]+1
+cumbounds(N) = sum(bsize(n) for n in 3:N)
+
